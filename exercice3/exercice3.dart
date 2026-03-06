ae9195e6cc5d14cd8050c4ad856e8f9669ed5726
TALIA-ASSEN <taliaassen02@gmail.com>
2026-03-06 20:19:36 +0100

    Add files via upload

---
+++ exercice3/exercice3.dart
@@ -0,0 +1,29 @@
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+}
+
+void main() {
+  List<Person> people = [
+    Person("joy", 25),
+    Person("talia", 30),
+    Person("albert", 35),
+    Person("Anna", 22),
+    Person("Bertrand", 28),
+    Person("Daniel", 40),
+    Person("Aimerode", 19),
+    Person("Eva-lyne", 33),
+  ];
+
+  List<Person> filtered = people
+      .where((p) => p.name.startsWith('A') || p.name.startsWith('B'))
+      .toList();
+
+  double averageAge = filtered.map((p) => p.age).reduce((a, b) => a + b) / filtered.length;
+
+  print("People starting with 'A' or 'B':");
+  filtered.forEach((p) => print("  ${p.name}: ${p.age}"));
+  print("\nAverage age: ${averageAge.toStringAsFixed(1)}");
+}
